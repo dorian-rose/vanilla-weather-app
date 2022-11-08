6c6e80457bbe730f0ee270efa0a10135291d617a
dorian-rose <116640452+dorian-rose@users.noreply.github.com>
2022-11-08 11:44:13 +0100

    Displayed last updated time and day

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   document.querySelector("#current-temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -12,8 +36,13 @@ function displayTemperature(response) {
   document.querySelector("#location").innerHTML = response.data.name;
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  document.querySelector("#date-time").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
+let city = "brisbane";
+let units = "metric";
 let apiKey = "535cacbb3f8a0df0aeb4790235b9541f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=adelaide&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(displayTemperature);
